e5ba649dc24b07be13d350835f3472b76c9bae98
vier <oliviervroege@hotmail.com>
2025-03-27 11:25:59 +0100

    feat: Add styling for blackjackgame

--- src/routes/blackjack/+page.svelte
+++ src/routes/blackjack/+page.svelte
@@ -2,6 +2,7 @@
     import { BlackjackGame } from "$lib/game/blackjack";
     import type { Card } from "$lib/api/deckAPI";
     import { GamePhase, PlayerAction } from "$lib/enums";
+    import { fade } from 'svelte/transition';;
 
     let game = new BlackjackGame();
 
@@ -110,124 +111,116 @@
         </div>
     </div>
     
-    <div class="bg-[#06561a] w-full rounded-md h-[50vh] p-4 shadow-2xl flex-col justify-start items-center">
+    <div class="bg-[#06561a] w-full rounded-md h-[50vh] p-4 shadow-2xl flex-col justify-start items-center gap-4">
 
-    {#if isGameOver}
-        <h2>Game Over! You are out of money.</h2>
-        <button onclick={restartGame}>Restart Game</button>
-    {/if}
+        {#if isGameOver}
+            <h2 in:fade={{delay:1000}}>Game Over! You are out of money.</h2>
+            <button in:fade={{delay:1000}} onclick={restartGame}>Restart Game</button>
+        {/if}
 
-    {#if currentPhase === GamePhase.Betting && !isGameOver}
-        <div class="flex justify-center items-center gap-4 w-full h-full">
-            <label class="bg-amber-700 p-2 rounded-md">
-                Bet:
-                <input
-                    type="number"
-                    bind:value={playerBet}
-                    min="10"
-                    step="10"
-                    max={playerBalance}
-                />
-            </label>
-            <button class="rounded-full bg-blue-950 p-4" onclick={placeBet} disabled={playerBet > playerBalance}
-                >Place Bet</button
-            >
-        </div>
-   
-    {/if}
-
-    {#if currentPhase !== GamePhase.Betting}
-    <div class="flex justify-center items-center gap-4 w-full h-full">
-
-        <section>
-            <div class="flex gap-4 justify-center items-center">
-            <h2 class="text-2xl border-white border-2 p-4 rounded-md m-4">Player's Hand ({playerHandValue})</h2>
-            <h2 class="text-2xl border-white border-2 p-4 rounded-md m-4">
-                Dealer's Hand ({currentPhase === GamePhase.Outcome
-                    ? dealerHandValue
-                    : "?"})
-            </h2>
-        </div>
-            <div class="flex gap-2">
-                {#each playerHand as card}
-                    <img
-                        src={card.image}
-                        alt={`${card.value} of ${card.suit}`}
-                        width="80"
+        {#if currentPhase === GamePhase.Betting && !isGameOver}
+            <div in:fade={{delay:1000}} class="flex justify-center items-center gap-4 w-full h-full">
+                <label class="bg-amber-700 p-2 rounded-md">
+                    Bet:
+                    <input
+                        type="number"
+                        bind:value={playerBet}
+                        min="10"
+                        step="10"
+                        max={playerBalance}
                     />
-                {/each}
-            </div>
-            {#if currentPhase === GamePhase.PlayerTurn}
-            <div class="flex gap-4 justify-center items-center">
-                
-            
-                <button onclick={() => performAction(PlayerAction.Hit)}
-                    >Hit</button
+                </label>
+                <button class="rounded-full bg-blue-950 p-4" onclick={placeBet} disabled={playerBet > playerBalance}
+                    >Place Bet</button
                 >
-                <button onclick={() => performAction(PlayerAction.Stand)}
-                    >Stand</button
-                >
-                {#if playerHand.length === 2 && playerBalance >= playerBet}
-                    <button
-                        class="rounded-full bg-blue-950 p-4"
-                        onclick={() => performAction(PlayerAction.DoubleDown)}
-                        >Double Down</button
-                    >
-                    <button
-                        class="rounded-full bg-blue-950 p-4"
-                        onclick={() => performAction(PlayerAction.Surrender)}
-                        >Surrender</button
-                    >
-                    {#if playerHand[0].value === playerHand[1].value}
-                        <button
-                            class="rounded-full bg-blue-950 p-4"
-                            onclick={() => performAction(PlayerAction.Split)}
-                            >Split</button
-                        >
-                    {/if}
-                {/if}
             </div>
+    
+        {/if}
 
-            {/if}
-        </section>
-
-        <section>
-      
-            <div class="flex gap-2">
-                {#if currentPhase === GamePhase.Outcome}
-                    {#each dealerHand as card}
+        {#if currentPhase !== GamePhase.Betting}
+            <div in:fade={{delay:1000}} class="flex gap-4 justify-center items-center">
+                <h2 class="text-2xl border-white border-2 p-4 rounded-md m-4">Player's Hand ({playerHandValue})</h2>
+                <h2 class="text-2xl border-white border-2 p-4 rounded-md m-4">
+                    Dealer's Hand ({currentPhase === GamePhase.Outcome
+                        ? dealerHandValue
+                        : "?"})
+                </h2>
+            </div>
+            <div in:fade={{delay:1000}} class="flex justify-center items-center gap-12 p-4">
+                <div class="flex justify-center gap-2">
+                    {#each playerHand as card}
                         <img
                             src={card.image}
                             alt={`${card.value} of ${card.suit}`}
                             width="80"
                         />
                     {/each}
-                {:else if dealerHand.length > 0}
-                    <!-- Show upcard and hide the hole card -->
-                    <img
-                        src={dealerHand[0].image}
-                        alt={`${dealerHand[0].value} of ${dealerHand[0].suit}`}
-                        width="80"
-                    />
-                    {#if dealerHand.length > 1}
-                        <img
-                            src="https://deckofcardsapi.com/static/img/back.png"
-                            alt="Hidden Card"
+                </div>
+                <div class="flex gap-2">
+                    {#if currentPhase === GamePhase.Outcome}
+                        {#each dealerHand as card}
+                            <img in:fade={{delay:1000}}
+                                src={card.image}
+                                alt={`${card.value} of ${card.suit}`}
+                                width="80"
+                            />
+                        {/each}
+                    {:else if dealerHand.length > 0}
+                        <!-- Show upcard and hide the hole card -->
+                        <img in:fade={{delay:1000}}
+                            src={dealerHand[0].image}
+                            alt={`${dealerHand[0].value} of ${dealerHand[0].suit}`}
                             width="80"
                         />
+                        {#if dealerHand.length > 1}
+                            <img
+                                src="https://deckofcardsapi.com/static/img/back.png"
+                                alt="Hidden Card"
+                                width="80"
+                            />
+                        {/if}
                     {/if}
-                {/if}
+                </div>
             </div>
-        </section>
-    </div>
-    {/if}
 
-    {#if currentPhase === GamePhase.Outcome && !isGameOver}
-        <section>
-            <h2>Game Outcome: {gameOutcome}</h2>
-            <button onclick={newRound}>New Round</button>
-        </section>
-    {/if}
-</div>
+            {#if currentPhase === GamePhase.PlayerTurn}
+                <div in:fade={{delay:1000}} class="flex gap-4 justify-center items-center">
+                    <button class="rounded-full bg-blue-950 p-4" onclick={() => performAction(PlayerAction.Hit)}
+                        >Hit</button 
+                    >
+                    <button class="rounded-full bg-blue-950 p-4" onclick={() => performAction(PlayerAction.Stand)}
+                        >Stand</button
+                    >
+                    {#if playerHand.length === 2 && playerBalance >= playerBet}
+                        <button
+                            class="rounded-full bg-blue-950 p-4"
+                            onclick={() => performAction(PlayerAction.DoubleDown)}
+                            >Double Down</button
+                        >
+                        <button
+                            class="rounded-full bg-red-950 p-4"
+                            onclick={() => performAction(PlayerAction.Surrender)}
+                            >Surrender</button
+                        >
+                        {#if playerHand[0].value === playerHand[1].value}
+                            <button
+                                class="rounded-full bg-blue-950 p-4"
+                                onclick={() => performAction(PlayerAction.Split)}
+                                >Split</button
+                            >
+                        {/if}
+                    {/if}
+                </div>
+            {/if}
+        
+
+        {/if}
+        {#if currentPhase === GamePhase.Outcome && !isGameOver}
+            <div in:fade={{delay:2000}} class="flex gap-4 justify-center items-center">
+                <h2 class="p-2 bg-amber-800 rounded-md">{gameOutcome}</h2>
+                <button class="bg-amber-700 p-2 rounded-md" onclick={newRound}>New Round</button>
+            </div>
+        {/if}
+    </div>
 
 </main>
